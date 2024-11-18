ecc711423a389b8b628d4968ef0db8ae81d781d4
안가은 <gaeun1021@sookmyung.ac.kr>
2024-11-18 21:37:38 +0900

    [Feat] HistoryView 구현

--- src/components/HistoryView.jsx
+++ src/components/HistoryView.jsx
@@ -1,22 +1,32 @@
-import "./HistoryView.css";
+import './HistoryView.css';
+import { useEffect, useState } from 'react';
 
 function HistoryView({ setView }) {
+  const answers = JSON.parse(localStorage.getItem('diary') || '{}');
+
   return (
     <>
-      <div style={{ display: "flex", alignItems: "center" }}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
         <button
-          className="back-btn"
+          className='back-btn'
           onClick={() => {
             // MainView 화면으로 전환
-            setView("main");
-          }}>
+            setView('main');
+          }}
+        >
           &lt;
         </button>
         <h4>다이어리 기록</h4>
       </div>
-      <div className="diary-item">
-        <div className="diary-date">(날짜)</div>
-        <div>(내용)</div>
+      <div className='diary-list'>
+        {Object.entries(answers).map(([date, answer]) => {
+          return (
+            <div key={date} className='diary-item'>
+              <div className='diary-date'>{date}일</div>
+              <div>{answer}</div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
